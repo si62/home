ac25ec7b72abfd69cdcb9ba317c34525ef6f84b2
Si <si62@msn.com>
2022-08-23 00:58:32 +0800

    Init project

---
+++ src/router.vue
@@ -0,0 +1,30 @@
+<script>
+import { createRouter, createWebHistory } from 'vue-router'
+import HelloWorld from './views/HelloWorld.vue'
+import Blank from './views/Blank.vue'
+import Modal from './views/Modal.vue'
+
+const routes = [
+    {
+        path : '/',
+        component : HelloWorld
+    },
+    {
+        path : '/blank',
+        component : Blank
+    },
+    {
+        path : '/modal',
+        component : Modal
+    }
+
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
+</script>
---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+<h1 class="text-3xl font-medium text-gray-700">Hello World</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
